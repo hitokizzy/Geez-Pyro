fddf7e5f3043dfe99d008bbe2778e2cd3ce2b81a
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-08 02:57:30 +0700

    Update apmguarde.py

--- geez/modules/private/apmguarde.py
+++ geez/modules/private/apmguarde.py
@@ -14,37 +14,37 @@ import geez.database.mongodb.pmpermitdb as geez
 async def pmguard(client, message):
     arg = get_arg(message)
     if not arg:
-        await message.edit("**I only understand on or off**")
+        await message.edit("**Apaan dah luh**")
         return
     if arg == "off":
         await geez.set_pm(False)
-        await message.edit("**PM Guard Deactivated**")
+        await message.edit("**PM Guard Mati Anjay**")
     if arg == "on":
         await geez.set_pm(True)
-        await message.edit("**PM Guard Activated**")
+        await message.edit("**PM Guard Aktif Anjay**")
 
 @gez.on_message(filters.command("setpmmsg", ["."]) & filters.me)
 async def setpmmsg(client, message):
     arg = get_arg(message)
     if not arg:
-        await message.edit("**What message to set**")
+        await message.edit("**Lah tau ya?**")
         return
     if arg == "default":
         await geez.set_permit_message(geez.PMPERMIT_MESSAGE)
-        await message.edit("**Anti_PM message set to default**.")
+        await message.edit("**Pesan anu ke anu**.")
         return
     await geez.set_permit_message(f"`{arg}`")
-    await message.edit("**Custom anti-pm message set**")
+    await message.edit("**Custom PM Guard Ke Set**")
 
 
 add_command_help(
     "PM Permit",
     [
-        [".pmguard [on or off]", " -> Activates or deactivates anti-pm."],
-        [".setpmmsg [message or default]", " -> Sets a custom anti-pm message."],
-        [".setblockmsg [message or default]", "-> Sets custom block message."],
-        [".setlimit [value]", " -> This one sets a max. message limit for unwanted PMs and when they go beyond it, bamm!."],
-        [".allow", " -> Allows a user to PM you."],
-        [".deny", " -> Denies a user to PM you."],
+        ["pmguard [on or off]", " -> Activates or deactivates anti-pm."],
+        ["setpmmsg [message or default]", " -> Sets a custom anti-pm message."],
+        ["setblockmsg [message or default]", "-> Sets custom block message."],
+        ["setlimit [value]", " -> This one sets a max. message limit for unwanted PMs and when they go beyond it, bamm!."],
+        ["ok|a|setuju", " -> Allows a user to PM you."],
+        ["no|tolak|disa", " -> Denies a user to PM you."],
     ],
 )
