91c8992969865c6b185d82d2b64cb177d16cdafb
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-08 01:04:25 +0700

    Update sudos.py

--- geez/modules/sudo/sudos.py
+++ geez/modules/sudo/sudos.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, errors, filters
 from pyrogram.types import ChatPermissions, Message
-DEVS = int(2003295492)
+from geez.helper.dev import *
 from geez.helper.PyroHelpers import get_ub_chats
 from geez.modules.basic.profile import extract_user, extract_user_and_reason
 from geez import SUDO_USER
@@ -88,7 +88,7 @@ async def gmute_user(client: Client, message: Message):
 
 
 add_command_help(
-    "sudos",
+    "Sudo",
     [
         [
             "addsudo <reply/username/userid>",
