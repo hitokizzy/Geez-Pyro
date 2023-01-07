ae9d9b45698c1ccf25798a3f6cd8f54c30a2a3e6
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-08 00:18:34 +0700

    Create sosmed.py

---
+++ geez/modules/fun/sosmed.py
@@ -0,0 +1,46 @@
+import asyncio
+from pyrogram.types import *
+from pyrogram import *
+from pyrogram import Client as gez
+from pyrogram import Client
+from geez.helper.cmd import *
+from geez.helper.basic import *
+from geez.helper.PyroHelpers import *
+from geez.helper.misc import *
+from geez.modules.help import *
+
+@gez.on_message(filters.command(["sosmed"], cmd) & filters.me)
+async def sosmed(client: Client, message: Message):
+    prik = await message.edit("`Bentar . . .`")
+    link = get_arg(message)
+    bot = "thisvidbot"
+    if link:
+        try:
+            tuyul = await client.send_message(bot, link)
+            await asyncio.sleep(5)
+            await tuyul.delete()
+        except YouBlockedUser:
+            await client.unblock_user(bot)
+            tuyul = await client.send_message(bot, link)
+            await asyncio.sleep(5)
+            await tuyul.delete()
+    async for sosmed in client.search_messages(
+        bot, filter=enums.MessagesFilter.VIDEO, limit=1
+    ):
+        await asyncio.gather(
+            prik.delete(),
+            client.send_video(
+                message.chat.id,
+                sosmed.video.file_id,
+                caption=f"**Upload by:** {client.me.mention}",
+                reply_to_message_id=ReplyCheck(message),
+            ),
+        )
+        await client.delete_messages(bot, 2)
+
+add_command_help(
+    "sosmed",
+    [
+        [f"sosmed <link>", ", to download media from Facebook/Tiktok/Instagram/Twitter/Youtube",],
+    ],
+)
