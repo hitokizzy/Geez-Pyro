19c1c7102454b3be108b0d9a821b5d66c3598f34
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-08 02:24:14 +0700

    Update weather.py

--- geez/modules/basic/weather.py
+++ geez/modules/basic/weather.py
@@ -8,7 +8,7 @@ from pyrogram.types import Message
 from geez.modules.help import add_command_help
 from pyrogram import enums
 
-@Client.on_message(filters.command(["weather", "w"], ".") & filters.me)
+@Client.on_message(filters.command(["cuaca", "cc"], ".") & filters.me)
 async def get_weather(bot: Client, message: Message):
     if len(message.command) == 1:
         await message.edit("Usage: `.weather Maldives`")
@@ -18,24 +18,24 @@ async def get_weather(bot: Client, message: Message):
     if len(message.command) > 1:
         location = message.command[1]
         headers = {"user-agent": "httpie"}
-        url = f"https://wttr.in/{location}?mnTC0&lang=en"
+        url = f"https://wttr.in/{location}?mnTC0&lang=id"
         try:
             async with aiohttp.ClientSession(headers=headers) as session:
                 async with session.get(url) as resp:
                     data = await resp.text()
         except Exception:
-            await message.edit("Failed to get the weather forecast")
+            await message.edit("Apaan tuh?")
 
         if "we processed more than 1M requests today" in data:
-            await message.edit("`Sorry, we cannot process this request today!`")
+            await message.edit("`Lah tau ya!`")
         else:
             weather = f"<code>{escape(data.replace('report', 'Report'))}</code>"
             await message.edit(weather, parse_mode=enums.ParseMode.MARKDOWN)
 
 
 add_command_help(
-    "weather",
+    "Cuaca",
     [
-        [".weather", "Gets weather information for provided location."],
+        ["cuaca|cc", "Gets weather information for provided location."],
     ],
 )
