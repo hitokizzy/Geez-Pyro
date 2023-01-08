e61dec00dae3f303553fd56b236981a1486c2728
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-09 00:18:19 +0700

    Update __init__.py

--- geez/helper/SQL/__init__.py
+++ geez/helper/SQL/__init__.py
@@ -4,13 +4,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from geez import MONGO_URL, LOGGER
+from geez import DB_URL, LOGGER
 
 SPAMBOT = "SPAMBOT"
 
 
 def start() -> scoped_session:
-    engine = create_engine(MONGO_URL)
+    engine = create_engine(DB_URL)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
@@ -21,7 +21,7 @@ try:
     SESSION = start()
 except AttributeError as e:
     LOGGER(__name__).warning(
-        "MONGO_URL is not configured. Features depending on the database might have issues."
+        "DB_URL is not configured. Features depending on the database might have issues."
     )
     LOGGER(__name__).info(str(e))
 
@@ -32,7 +32,7 @@ BOTINLINE_AVAIABLE = False
 
 def mulaisql() -> scoped_session:
     global DB_AVAILABLE
-    engine = create_engine(MONGO_URL, client_encoding="utf8")
+    engine = create_engine(DB_URL, client_encoding="utf8")
     BASE.metadata.bind = engine
     try:
         BASE.metadata.create_all(engine)
