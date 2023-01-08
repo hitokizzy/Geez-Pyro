c11966c171ee94f21a3c9ababea29bb0574083c4
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-08 16:05:26 +0700

    Update __init__.py

--- geez/__init__.py
+++ geez/__init__.py
@@ -1,6 +1,9 @@
 import logging
 import time
 import asyncio
+import asyncio
+from pytgcalls import idle
+from config import call_py, bot
 from pyrogram import Client
 from config import API_ID, API_HASH, SUDO_USERS, OWNER_ID, BOT_TOKEN, STRING_SESSION1, STRING_SESSION2, STRING_SESSION3, STRING_SESSION4, STRING_SESSION5, STRING_SESSION6, STRING_SESSION7, STRING_SESSION8, STRING_SESSION9, STRING_SESSION10, BOTLOG_CHATID
 from datetime import datetime
@@ -47,6 +50,23 @@ logging.getLogger("pyrogram.session.auth").setLevel(logging.CRITICAL)
 logging.getLogger("pyrogram.session.session").setLevel(logging.CRITICAL)
 
 LOGS = logging.getLogger(__name__)
+
+
+async def main():
+    print("STARTING UBOT CLIENT")
+    await bot.start()
+    print("STARTING PYTGCALLS CLIENT")
+    await call_py.start()
+    print(
+        """
+    ------------------------
+   | Geez-Pyro Actived! |
+    ------------------------
+"""
+    )
+    await idle()
+    print("STOPPING USERBOT")
+    await bot.stop()
 
 
 def LOGGER(name: str) -> logging.Logger:
@@ -127,3 +147,7 @@ if STRING_SESSION10:
    clients.append(client10)
 
 client = [client for client in[STRING_SESSION1, STRING_SESSION2, STRING_SESSION3, STRING_SESSION4, STRING_SESSION5]if client]
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main())
