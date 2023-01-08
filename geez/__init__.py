778070db1c24cad6a307a60caca960d30ed6d114
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-08 23:15:10 +0700

    Update __init__.py

--- geez/__init__.py
+++ geez/__init__.py
@@ -3,7 +3,6 @@ import time
 import asyncio
 
 from pyrogram import Client
-from config import API_ID, API_HASH, SUDO_USERS, OWNER_ID, BOT_TOKEN, STRING_SESSION1, STRING_SESSION2, STRING_SESSION3, STRING_SESSION4, STRING_SESSION5, STRING_SESSION6, STRING_SESSION7, STRING_SESSION8, STRING_SESSION9, STRING_SESSION10, BOTLOG_CHATID
 from datetime import datetime
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
