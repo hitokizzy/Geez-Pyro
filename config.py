3ae18ea173f1d9d63bb30d2d46688c069a9aa5ed
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-08 22:22:29 +0700

    Update config.py

--- config.py
+++ config.py
@@ -10,6 +10,10 @@ if os.path.exists(".env"):
 API_ID = getenv("API_ID")
 API_HASH = getenv("API_HASH")
 STRING_SESSION1 = getenv("STRING_SESSION1")
+BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
+if not BLACKLIST_CHAT:
+    BLACKLIST_CHAT = = [-1001692751821, -1001675396283, -1001578091827, -1001473548283, -1001459812644]
+BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split()}
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
 OWNER_ID = getenv("OWNER_ID", "2003295492")
 BOT_TOKEN = getenv("BOT_TOKEN")
@@ -17,7 +21,7 @@ MONGO_URL = getenv("MONGO_URL")
 ALIVE_PIC = getenv("ALIVE_PIC", "https://telegra.ph/file/c78bb1efdeed38ee16eb2.png")
 ALIVE_TEXT = getenv("ALIVE_TEXT", "")
 PM_LOGGER = getenv("PM_LOGGER")
-BOTLOG_CHATID = getenv("BOTLOG_CHATID")
+BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
 REPO_URL = getenv("REPO_URL", "https://github.com/hitokizzy/Geez-Pyro")
 BRANCH = getenv("BRANCH", "main") #don't change
