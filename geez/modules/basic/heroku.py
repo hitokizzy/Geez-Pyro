a0ef7eb4815ef40b61c60e660a8d61172a0589cf
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-10 01:29:43 +0700

    Update heroku.py

--- geez/modules/basic/heroku.py
+++ geez/modules/basic/heroku.py
@@ -1,6 +1,5 @@
 import asyncio
 import math
-import sys
 
 import dotenv
 import heroku3
@@ -10,44 +9,44 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 from config import *
-from geez.helper import restart
 from geez.helper.basic import edit_or_reply
 from geez.helper.misc import HAPP, in_heroku
-from geez.modules.help import add_command_help
+from geez.utils.misc import restart
 
+from .help import add_command_help
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-    
-    
+
+
 @Client.on_message(filters.command("setvar", CMD_HANDLER) & filters.me)
 async def set_var(client: Client, message: Message):
     if len(message.command) < 3:
         return await edit_or_reply(
             message, f"<b>Usage:</b> {CMD_HANDLER}setvar [Var Name] [Var Value]"
         )
-    Man = await edit_or_reply(message, "`Processing...`")
+    Geez = await edit_or_reply(message, "`Processing...`")
     to_set = message.text.split(None, 2)[1].strip()
     value = message.text.split(None, 2)[2].strip()
     if await in_heroku():
         if HAPP is None:
-            return await Man.edit(
+            return await Geez.edit(
                 "Pastikan HEROKU_API_KEY dan HEROKU_APP_NAME anda dikonfigurasi dengan benar di config vars heroku"
             )
         heroku_config = HAPP.config()
         if to_set in heroku_config:
-            await Man.edit(f"Berhasil Mengubah var {to_set} menjadi {value}")
+            await Geez.edit(f"Berhasil Mengubah var {to_set} menjadi {value}")
         else:
-            await Man.edit(f"Berhasil Menambahkan var {to_set} menjadi {value}")
+            await Geez.edit(f"Berhasil Menambahkan var {to_set} menjadi {value}")
         heroku_config[to_set] = value
     else:
         path = dotenv.find_dotenv("config.env")
         if not path:
-            return await Man.edit(".env file not found.")
+            return await Geez.edit(".env file not found.")
         dotenv.set_key(path, to_set, value)
         if dotenv.get_key(path, to_set):
-            await Man.edit(f"Berhasil Mengubah var {to_set} menjadi {value}")
+            await Geez.edit(f"Berhasil Mengubah var {to_set} menjadi {value}")
         else:
-            await Man.edit(f"Berhasil Menambahkan var {to_set} menjadi {value}")
+            await Geez.edit(f"Berhasil Menambahkan var {to_set} menjadi {value}")
         restart()
 
 
@@ -57,40 +56,40 @@ async def varget_(client: Client, message: Message):
         return await edit_or_reply(
             message, f"<b>Usage:</b> {CMD_HANDLER}getvar [Var Name]"
         )
-    Man = await edit_or_reply(message, "`Processing...`")
+    Geez = await edit_or_reply(message, "`Processing...`")
     check_var = message.text.split(None, 2)[1]
     if await in_heroku():
         if HAPP is None:
-            return await Man.edit(
+            return await Geez.edit(
                 "Pastikan HEROKU_API_KEY dan HEROKU_APP_NAME anda dikonfigurasi dengan benar di config vars heroku"
             )
         heroku_config = HAPP.config()
         if check_var in heroku_config:
-            return await Man.edit(
+            return await Geez.edit(
                 f"<b>{check_var}:</b> <code>{heroku_config[check_var]}</code>"
             )
         else:
-            return await Man.edit(f"Tidak dapat menemukan var {check_var}")
+            return await Geez.edit(f"Tidak dapat menemukan var {check_var}")
     else:
         path = dotenv.find_dotenv("config.env")
         if not path:
-            return await Man.edit(".env file not found.")
+            return await Geez.edit(".env file not found.")
         output = dotenv.get_key(path, check_var)
         if not output:
-            await Man.edit(f"Tidak dapat menemukan var {check_var}")
+            await Geez.edit(f"Tidak dapat menemukan var {check_var}")
         else:
-            return await Man.edit(f"<b>{check_var}:</b> <code>{str(output)}</code>")
+            return await Geez.edit(f"<b>{check_var}:</b> <code>{str(output)}</code>")
 
 
 @Client.on_message(filters.command("delvar", CMD_HANDLER) & filters.me)
 async def vardel_(client: Client, message: Message):
     if len(message.command) != 2:
         return await message.edit(f"<b>Usage:</b> {CMD_HANDLER}delvar [Var Name]")
-    Man = await edit_or_reply(message, "`Processing...`")
+    Geez = await edit_or_reply(message, "`Processing...`")
     check_var = message.text.split(None, 2)[1]
     if await in_heroku():
         if HAPP is None:
-            return await Man.edit(
+            return await Geez.edit(
                 "Pastikan HEROKU_API_KEY dan HEROKU_APP_NAME anda dikonfigurasi dengan benar di config vars heroku"
             )
         heroku_config = HAPP.config()
@@ -159,8 +158,8 @@ async def usage_heroku(client: Client, message: Message):
     AppHours = math.floor(AppQuotaUsed / 60)
     AppMinutes = math.floor(AppQuotaUsed % 60)
     await asyncio.sleep(1.5)
-    text =f"""
-𝗜𝗡𝗙𝗢 𝗞𝗘𝗞𝗨𝗔𝗧𝗔𝗡 𝗥𝗔𝗠-𝗨𝗕𝗢𝗧!!
+    text = f"""
+𝗜𝗡𝗙𝗢 𝗞𝗘𝗞𝗨𝗔𝗧𝗔𝗡 𝗚𝗲𝗲𝘇-𝗣𝘆𝗿𝗼!!
 
 ╭✠╼━━━━━━❖━━━━━━━✠╮
 ┣•𝗣𝗘𝗡𝗚𝗚𝗨𝗡𝗔𝗔𝗡 𝗦𝗔𝗔𝗧 𝗜𝗡𝗜 : 
@@ -193,10 +192,20 @@ async def usange_heroku(client: Client, message: Message):
         "\n╚════════════════════╝\n"
     )
 
+
 add_command_help(
-    "Heroku",
+    "heroku",
     [
-        [".usage", "gatau."],
-        [".setvar", "gatau."],
+        ["setvar", "Untuk mengatur variabel config userbot."],
+        ["delvar", "Untuk menghapus variabel config userbot."],
+        ["getvar", "Untuk melihat variabel config userbot."],
+        [
+            f"usage atau {CMD_HANDLER}dyno",
+            "Untuk mengecheck kouta dyno heroku.",
+        ],
+        [
+            "usange",
+            "Fake Usage Kouta Dyno Heroku jadi 1000jam Untuk menipu temanmu wkwk.",
+        ],
     ],
 )
