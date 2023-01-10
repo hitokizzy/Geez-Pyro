99fb9cb64d7a398ab1dbccab03afaf45184491fc
izzy <50057075+hitokizzy@users.noreply.github.com>
2023-01-10 19:25:52 +0700

    Update __main__.py

--- geez/__main__.py
+++ geez/__main__.py
@@ -8,14 +8,14 @@ from geez import clients, ids, app
 from geez.helper.error import *
 from geez.helper.misc import heroku
 from config import *
-
+from geez import LOGGER
 
 async def start_bot():
     await app.start()
-    print("LOG: Mendirikan Bot token Booting..")
+    print("LOG: Memulai Geez Pyro..")
     for all_module in ALL_MODULES:
         importlib.import_module("geez.modules" + all_module)
-        print(f"Successfully Imported {all_module} 🛠️")
+        print(f"Successfully Imported {all_module} ")
     for cli in clients:
         try:
             await cli.start()
@@ -25,7 +25,7 @@ async def start_bot():
                 await cli.send_photo(LOG_GROUP, photo=LOG_ALIVE, caption=ALIVE_ONLINE)
             except BaseException:
                 pass
-            print(f"Started {ex.first_name} 🛠️")
+            print(f"Started {ex.first_name} ")
             ids.append(ex.id)
         except Exception as e:
             print(f"{e}")
