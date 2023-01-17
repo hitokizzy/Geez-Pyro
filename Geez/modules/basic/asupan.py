0f6df93d19816c41fcb3f3c489f30be7eb1b09c6
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-17 23:30:05 +0700

    Update asupan.py

--- Geez/modules/basic/asupan.py
+++ Geez/modules/basic/asupan.py
@@ -15,7 +15,7 @@ from pyrogram import Client, filters, enums
 from pyrogram.types import ChatPermissions, ChatPrivileges, Message
 from pyrogram import Client as gez 
 from geezlibs.geez.helper import edit_or_reply, get_text, ReplyCheck
-from Geez.helper.cmd import *
+from geezlibs.geez.helper.cmd import *
 from geezlibs import DEVS, BL_GCAST
 from Geez.modules.basic import add_command_help
 from config import *
