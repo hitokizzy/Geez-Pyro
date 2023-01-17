bf6ae1942cf55042a61daaf0066af61a2c4eb211
vckyou <79180058+vckyou@users.noreply.github.com>
2023-01-17 19:57:52 +0700

    Update __main__.py

--- Geez/__main__.py
+++ Geez/__main__.py
@@ -31,7 +31,7 @@ async def main():
             ex = await bot.get_me()
             await join(bot)
             try:
-                await bot.send_message(BOTLOG_CHATID, MSG_ON.format(BOT_VER, CMD_HNDLR, gver))
+                await bot.send_message(BOTLOG_CHATID, MSG_ON.format(BOT_VER, gver, CMD_HNDLR))
             except BaseException:
                 pass
             print(f"Started as {ex.first_name} | {ex.id} ")
