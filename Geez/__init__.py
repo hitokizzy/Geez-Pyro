7a0fe5681b978196aa5c4dea9155f9b1e4ff8179
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-17 23:21:11 +0700

    Update __init__.py

--- Geez/__init__.py
+++ Geez/__init__.py
@@ -5,6 +5,8 @@ import time
 from datetime import datetime
 from logging.handlers import RotatingFileHandler
 from typing import Any, Dict
+import motor.motor_asyncio
+from .config_var import Config
 
 from aiohttp import ClientSession
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
@@ -47,6 +49,8 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 logging.getLogger("pyrogram.client").setLevel(logging.WARNING)
 logging.getLogger("pyrogram.session.auth").setLevel(logging.CRITICAL)
 logging.getLogger("pyrogram.session.session").setLevel(logging.CRITICAL)
+
+mongo_client = motor.motor_asyncio.AsyncIOMotorClient(Config.MONGO_URL)
 
 LOGS = logging.getLogger(__name__)
 
