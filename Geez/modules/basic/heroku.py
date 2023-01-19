ac64d977bf99f3c20b09ce0010a05ac935393eaa
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-20 00:17:47 +0700

    Update heroku.py

--- Geez/modules/basic/heroku.py
+++ Geez/modules/basic/heroku.py
@@ -50,6 +50,38 @@ XCB = [
     "main",
 ]
 
+@Client.on_message(filters.command("restart", cmd) & filters.me)
+async def restart_bot(_, message: Message):
+    try:
+        msg = await message.reply(" `Restarting bot...`")
+        LOGGER(__name__).info("BOT SERVER RESTARTED !!")
+    except BaseException as err:
+        LOGGER(__name__).info(f"{err}")
+        return
+    await msg.edit_text("✅ **Bot has restarted !**\n\n")
+    if HAPP is not None:
+        HAPP.restart()
+    else:
+        args = [sys.executable, "-m", "Geez"]
+        execle(sys.executable, *args, environ)
+
+
+@Client.on_message(
+    filters.command(["shutdown", "off"], cmd) & filters.me
+)
+async def shutdown_bot(client: Client, message: Message):
+    if BOTLOG_CHATID:
+        await client.send_message(
+            BOTLOG_CHATID,
+            "**#SHUTDOWN** \n"
+            "**Geez-Userbot** telah di matikan!\nJika ingin menghidupkan kembali silahkan buka heroku",
+        )
+    await message.reply("🔌 **Geez-Userbot Berhasil di matikan!**")
+    if HAPP is not None:
+        HAPP.process_formation()["worker"].scale(0)
+    else:
+        sys.exit(0)
+
 
 @Client.on_message(
     filters.command(["logs"], ".") & (filters.me | filters.user(SUDO_USER))
