749b76cbd961c1cb0ed0281855e5d3a991b332fe
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-20 00:18:58 +0700

    Update heroku.py

--- Geez/modules/basic/heroku.py
+++ Geez/modules/basic/heroku.py
@@ -28,6 +28,7 @@ from pyrogram.types import Message
 
 from config import BOTLOG_CHATID, HEROKU_API_KEY, HEROKU_APP_NAME, BRANCH, REPO_URL
 from config import CMD_HNDLR as cmds
+from geezlibs.geez.helper.cmd import *
 from Geez import SUDO_USER, Client
 from Geez.modules.basic import add_command_help
 
