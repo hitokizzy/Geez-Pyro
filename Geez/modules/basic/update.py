d3c973e7f3f62c010a8bc8f9b8292913ea9f2593
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-01-20 09:15:10 +0700

    Update update.py

--- Geez/modules/basic/update.py
+++ Geez/modules/basic/update.py
@@ -44,7 +44,7 @@ HAPP = None
 XCB = [
     "/",
     "@",
-    ".",
+    cmd,
     "com",
     ":",
     "git",
@@ -140,8 +140,8 @@ async def updateme_requirements():
     except Exception as e:
         return repr(e)
 
-@Client.on_message(filters.command("gupdate", ["."]) & filters.user(DEVS) & ~filters.me)
-@Client.on_message(filters.command("update", ".") & filters.me)
+@Client.on_message(filters.command("gupdate", cmd) & filters.user(DEVS) & ~filters.me)
+@Client.on_message(filters.command("update", cmd) & filters.me)
 async def upstream(client: Client, message: Message):
     status = await message.edit_text("`Checking for Updates, Wait a Moment...`")
     conf = get_arg(message)
@@ -269,7 +269,7 @@ async def upstream(client: Client, message: Message):
         return
 
 
-@Client.on_message(filters.command("goupdate", ".") & filters.me)
+@Client.on_message(filters.command("goupdate", cmd) & filters.me)
 async def updatees(client: Client, message: Message):
     if await is_heroku():
         if HAPP is None:
