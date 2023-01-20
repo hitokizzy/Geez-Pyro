5652df9ed9397b5119ced881704e7745dfa584b1
izzy <50057075+hitokizzy@users.noreply.github.com>
2023-01-20 18:52:44 +0700

    Decepticon : Auto Improve

--- Geez/modules/basic/help.py
+++ Geez/modules/basic/help.py
@@ -65,7 +65,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"**Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @GeezSupport >< @ramsupportt"
+            this_command += "© @GeezRam >< @UserbotCh"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
@@ -92,7 +92,7 @@ async def module_helper(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @GeezSupport >< @ramsupportt •"
+            message, f"```{str(ac)}```\n• @GeezRam >< @UserbotCh •"
         )
         await message.reply(
             f"**Usage**:`.help broadcast` **To View Module details**"
@@ -104,7 +104,7 @@ async def module_helper(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @TheUpdatesChannel"
+            this_command += "© @GeezRam >< @UserbotCh"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
