88b5803648ba7e177093d41c0898f326835266eb
vckyou <79180058+vckyou@users.noreply.github.com>
2023-01-26 08:24:50 +0000

    Decepticon : Auto Fix

--- Geez/modules/basic/help.py
+++ Geez/modules/basic/help.py
@@ -65,7 +65,7 @@ async def module_help(client: Client, message: Message):
             commands: dict = CMD_HELP[help_arg]
             this_command = f"**Help For {str(help_arg).upper()}**\n\n"
             for x in commands:
-                this_command += f"**Command:** `.{str(x)}`\n  ∟ **Function:** `{str(commands[x])}`\n\n"
+                this_command += f"**Command:** `{str(x)}`\n  ∟ **Function:** `{str(commands[x])}`\n\n"
             this_command += "© @GeezRam >< @UserbotCh"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
@@ -104,7 +104,7 @@ async def module_helper(client: Client, message: Message):
             commands: dict = CMD_HELP[help_arg]
             this_command = f"**Help For {str(help_arg).upper()}**\n\n"
             for x in commands:
-                this_command += f"**Command:** `.{str(x)}`\n  ∟ **Function:** `{str(commands[x])}`\n\n"
+                this_command += f"**Command:** `{str(x)}`\n  ∟ **Function:** `{str(commands[x])}`\n\n"
             this_command += "© @GeezSupport >< @ramsupportt"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
