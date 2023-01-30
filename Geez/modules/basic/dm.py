90c4b294fb037bca8b0cad6ec9469703465d33ed
izzy-adeeva <88928279+izzy-adeeva@users.noreply.github.com>
2023-01-31 02:55:22 +0700

    Update dm.py

--- Geez/modules/basic/dm.py
+++ Geez/modules/basic/dm.py
@@ -3,9 +3,9 @@ from pyrogram.types import Message
 from Geez.modules.basic import add_command_help
 from Geez import cmds
 
-@Client.on_message(filters.command(["directmessage", "dm"], cmds) & filters.me)
+@Client.on_message(filters.command(["directmessage", "dm"], cmd) & filters.me)
 async def dm(coli: Client, memek: Message):
-    geez = await memek.reply_text("⚡ Processing.....")
+    geez = await memek.reply_text("⚡ Proccessing.....")
     quantity = 1
     inp = memek.text.split(None, 2)[1]
     user = await coli.get_chat(inp)
@@ -17,7 +17,7 @@ async def dm(coli: Client, memek: Message):
         for _ in range(quantity):
             await geez.edit("Message Sended Successfully 😘")
             await coli.send_message(user.id, spam_text,
-                                      reply_to_message_id=reply_to_id)
+                                      reply_to_messsge_id=reply_to_id)
             await asyncio.sleep(0.15)
         return
 
