246a4a6168ef2db1057cd34aa07db980fcebb793
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-02-04 00:58:50 +0700

    Create yt.py

---
+++ Geez/modules/basic/yt.py
@@ -0,0 +1,210 @@
+# if you can read this, this meant you use code from Geez Ram Project
+# this code is from somewhere else
+# please dont hestitate to steal it
+# because Geez and Ram doesn't care about credit
+# at least we are know as well
+# who Geez and Ram is
+#
+#
+# kopas repo dan hapus credit, ga akan jadikan lu seorang developer
+# ©2023 Geez & Ram Team
+
+import asyncio
+import os
+import time
+from datetime import datetime
+
+import wget
+from pyrogram import *
+from pyrogram.types import *
+from youtubesearchpython import SearchVideos
+from yt_dlp import YoutubeDL
+
+from Geez.modules.basic import add_command_help
+from Geez import *
+from geezlibs.geez.helper.PyroHelpers import ReplyCheck
+from geezlibs.geez.utils.tools import get_arg
+
+
+def get_text(message: Message) -> [None, str]:
+    """Extract Text From Commands"""
+    text_to_return = message.text
+    if message.text is None:
+        return None
+    if " " in text_to_return:
+        try:
+            return message.text.split(None, 1)[1]
+        except IndexError:
+            return None
+    else:
+        return None
+
+
+@Client.on_message(
+    filters.command(["vid", "video"], cmds) & filters.me
+)
+async def yt_vid(client: Client, message: Message):
+    input_st = message.text
+    input_str = input_st.split(" ", 1)[1]
+    Geez = await message.reply(" `Processing...`")
+    if not input_str:
+        await Geez.edit_text(
+            "`Please Give Me A Valid Input. You Can Check Help Menu To Know More!`"
+        )
+        return
+    await Geez.edit_text(f"`Searching {input_str} From Youtube. Please Wait.`")
+    search = SearchVideos(str(input_str), offset=1, mode="dict", max_results=1)
+    rt = search.result()
+    result_s = rt["search_result"]
+    url = result_s[0]["link"]
+    vid_title = result_s[0]["title"]
+    yt_id = result_s[0]["id"]
+    uploade_r = result_s[0]["channel"]
+    thumb_url = f"https://img.youtube.com/vi/{yt_id}/hqdefault.jpg"
+    await asyncio.sleep(0.6)
+    downloaded_thumb = wget.download(thumb_url)
+    opts = {
+        "format": "best",
+        "addmetadata": True,
+        "key": "FFmpegMetadata",
+        "prefer_ffmpeg": True,
+        "geo_bypass": True,
+        "nocheckcertificate": True,
+        "postprocessors": [{"key": "FFmpegVideoConvertor", "preferedformat": "mp4"}],
+        "outtmpl": "%(id)s.mp4",
+        "logtostderr": False,
+        "quiet": True,
+    }
+    try:
+        with YoutubeDL(opts) as ytdl:
+            ytdl_data = ytdl.extract_info(url, download=True)
+    except Exception as e:
+        await Geez.edit_text(f"**Failed To Download** \n**Error :** `{str(e)}`")
+        return
+    time.time()
+    file_path = f"{ytdl_data['id']}.mp4"
+    capy = f"🏷 **Video Name ►** `{vid_title}` \n🧸 **Requested For ►** `{input_str}` \n💌 **Channel ►** `{uploade_r}` \n**Link ►** `{url}`"
+    await client.send_video(
+        message.chat.id,
+        video=open(file_path, "rb"),
+        duration=int(ytdl_data["duration"]),
+        file_name=str(ytdl_data["title"]),
+        thumb=downloaded_thumb,
+        caption=capy,
+        supports_streaming=True,
+    )
+    await Geez.delete()
+    for files in (downloaded_thumb, file_path):
+        if files and os.path.exists(files):
+            os.remove(files)
+
+
+@Client.on_message(filters.command("song", cmds) & filters.me)
+async def song(client: Client, message: Message):
+    input_str = get_text(message)
+    rep = await message.reply("`Processing...`")
+    if not input_str:
+        await rep.edit(
+            "`Please Give Me A Valid Input. You Can Check Help Menu To Know More!`"
+        )
+        return
+    await rep.edit(f"`Getting {input_str} From Youtube Servers. Please Wait.`")
+    search = SearchVideos(str(input_str), offset=1, mode="dict", max_results=1)
+    rt = search.result()
+    result_s = rt["search_result"]
+    url = result_s[0]["link"]
+    vid_title = result_s[0]["title"]
+    yt_id = result_s[0]["id"]
+    uploade_r = result_s[0]["channel"]
+    thumb_url = f"https://img.youtube.com/vi/{yt_id}/hqdefault.jpg"
+    await asyncio.sleep(0.6)
+    downloaded_thumb = wget.download(thumb_url)
+    opts = {
+        "format": "bestaudio",
+        "addmetadata": True,
+        "key": "FFmpegMetadata",
+        "writethumbnail": True,
+        "prefer_ffmpeg": True,
+        "geo_bypass": True,
+        "nocheckcertificate": True,
+        "postprocessors": [
+            {
+                "key": "FFmpegExtractAudio",
+                "preferredcodec": "mp3",
+                "preferredquality": "720",
+            }
+        ],
+        "outtmpl": "%(id)s.mp3",
+        "quiet": True,
+        "logtostderr": False,
+    }
+    try:
+        with YoutubeDL(opts) as ytdl:
+            ytdl_data = ytdl.extract_info(url, download=True)
+    except Exception as e:
+        await rep.edit(f"**Failed To Download** \n**Error :** `{str(e)}`")
+        return
+    time.time()
+    file_sung = f"{ytdl_data['id']}.mp3"
+    capy = f"**🏷 Song Name ►** `{vid_title}` \n🧸 **Requested For ►** `{input_str}` \n💌 **Channel ►** `{uploade_r}` \n📎 **Link ►** `{url}`"
+    await client.send_audio(
+        message.chat.id,
+        audio=open(file_sung, "rb"),
+        title=str(ytdl_data["title"]),
+        performer=str(ytdl_data["uploader"]),
+        thumb=downloaded_thumb,
+        caption=capy,
+    )
+    await rep.delete()
+    for files in (downloaded_thumb, file_sung):
+        if files and os.path.exists(files):
+            os.remove(files)
+            
+            
+@Client.on_message(filters.command(["tt", "tiktok", "ig", "sosmed"], cmds) & filters.me)
+async def sosmed(client: Client, message: Message):
+    prik = await message.edit("`Processing . . .`")
+    link = get_arg(message)
+    bot = "thisvidbot"
+    if link:
+        try:
+            tuyul = await client.send_message(bot, link)
+            await asyncio.sleep(5)
+            await tuyul.delete()
+        except YouBlockedUser:
+            await client.unblock_user(bot)
+            tuyul = await client.send_message(bot, link)
+            await asyncio.sleep(5)
+            await tuyul.delete()
+    async for sosmed in client.search_messages(
+        bot, filter=enums.MessagesFilter.VIDEO, limit=1
+    ):
+        await asyncio.gather(
+            prik.delete(),
+            client.send_video(
+                message.chat.id,
+                sosmed.video.file_id,
+                caption=f"**Upload by:** {client.me.mention}",
+                reply_to_message_id=ReplyCheck(message),
+            ),
+        )
+        await client.delete_messages(bot, 2)
+
+
+add_command_help(
+    "Youtube",
+    [
+        ["song <title>", "Download Audio From YouTube."],
+        ["video <title>", "Download Video from YouTube."],
+    ],
+)
+
+add_command_help(
+    "Sosmed",
+    [
+        [
+            f"sosmed <link>",
+            "Untuk Mendownload Media Dari Facebook / Tiktok / Instagram / Twitter / YouTube.",
+        ],
+    ],
+)
